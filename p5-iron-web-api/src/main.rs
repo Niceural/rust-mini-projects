579b37e1462724c4f8e1bce2afda3891491e96c1
Niceural <nicolas.bayle20@imperial.ac.uk>
2022-07-11 21:41:02 +0200

    first commit

---
+++ p5-iron-web-api/src/main.rs
@@ -0,0 +1,12 @@
+extern crate chrono;
+extern crate env_logger;
+extern crate iron;
+extern crate logger;
+extern crate router;
+extern crate serde;
+
+mod models;
+
+fn main() {
+    println!("Hello, world!");
+}
